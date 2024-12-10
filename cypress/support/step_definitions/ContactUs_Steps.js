401778034720f0b982a061b2f865c4aa0bc219c3
Manos <manosmakromallis@gmail.com>
2024-12-10 17:52:50 +0200

    initial commit

---
+++ cypress/support/step_definitions/ContactUs_Steps.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+import { When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+When('I type a first name', () => {
+    cy.get('[name="first_name"]')
+    .type('Joe')
+})
+
+When('I type a last name', () => {
+
+})
+
+When('I enter an email', () => {
+
+})
+
+When('I type a comment', () => {
+
+})
+
+When('I click on the submit button', () => {
+
+})
+
+Then('I should be presented with a successful contact us submission message', () => {
+
+})
